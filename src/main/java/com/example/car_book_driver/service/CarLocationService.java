4c7a386b83e2bf554d3ec416850b60bc6e1d791f
Ravindu Thakshila Jayasooriya <62090384+RavinduJP@users.noreply.github.com>
2025-03-20 01:18:32 +0530

    add service annotation

--- src/main/java/com/example/car_book_driver/service/CarLocationService.java
+++ src/main/java/com/example/car_book_driver/service/CarLocationService.java
@@ -2,7 +2,9 @@ package com.example.car_book_driver.service;
 
 import com.example.car_book_driver.constant.AppConstant;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CarLocationService {
 
     private KafkaTemplate<String, Object> kafkaTemplate;
